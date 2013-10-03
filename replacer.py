270b646b0952b0b9147648b60a08acd77b6c2216
dmitri777 <dmitri.titov@gmail.com>
2013-10-03 19:19:43 +0200

    adding directory rename

--- replacer.py
+++ replacer.py
@@ -4,26 +4,48 @@ import unidecode
 
 orig = sys.argv[1]
 
+def nameChange(name):
+
+    newFileName = unidecode.unidecode(unicode(name))
+    newFileName = newFileName.replace(" ", "_")
+    newFileName = re.sub(r'(;|\:|\!|\?|\,)', "", newFileName)
+
+    return newFileName
+
+
 if not os.path.exists(orig):
 	print("no folder to process")
 	sys.exit(0)
 
+# first files
 for root, dir, files in os.walk(unicode(orig)):
     
-    path = root.split('/')
-
     for file in files:
 
         originalFullName = os.path.join(root, file)
 
-        newFileName = unidecode.unidecode(unicode(file))
-        newFileName = newFileName.replace(" ", "_")
-        newFileName = re.sub(r'(;|\:|\!|\?|\,)', "", newFileName)
+        newFileName = nameChange(file)
         newFullname = os.path.join(root, newFileName)
-
 
         if originalFullName != newFullname:
             os.rename(originalFullName, newFullname)
-            print("Renamed file " + repr(originalFullName) + " into " + newFullname)
+            print("Renamed file " + repr(originalFullName) + " into " + repr(newFullname))
 
 
+# now directories
+while True:
+
+    renameDone = False
+
+    for root, dir, files in os.walk(unicode(orig)):
+        for d in dir:
+            newDirName = nameChange(d)
+            if newDirName != d:
+
+                fullDirName = os.path.join(root, d)
+                newFullDirName = os.path.join(root, newDirName)
+                os.rename(fullDirName, newFullDirName)
+                print("Renamed directory from " + repr(fullDirName) + " to "  + repr(newFullDirName))
+                renameDone = True
+          
+    if renameDone == False: break            
